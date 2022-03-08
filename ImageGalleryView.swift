d71a7011d05cde1be545aed2df9b61bc88a07a2d
Cristina Cernat <cernat.cristina@yahoo.com>
2022-03-08 13:24:56 +0200

    Initial commit

---
+++ ImageGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageGalleryView.swift
+//  ImageGallery
+//
+//  Created by Cristina Mihaela on 08.03.2022.
+//
+
+import UIKit
+
+class ImageGalleryView: UIView {
+
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+
+}
---
+++ ImageGalleryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ImageGalleryViewController.swift
+//  ImageGallery
+//
+//  Created by Cristina Mihaela on 08.03.2022.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UIViewController {
+
+
+
+    @IBOutlet weak var dropZone: UIView!
+    
+}
